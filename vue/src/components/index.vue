5a7c0894e6f6991e191489800bd423266e90be6c
liuqingcun <liuqingcun_code@163.com>
2019-05-21 18:52:11 +0800

    vue

---
+++ vue/src/components/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="linkList">
+    <router-link :to="{ path:'/preImg'}"  tag="p">
+      <div class="linkItem">图片预加载问题</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+.linkList {
+  font-size: 24px;
+}
+</style>
