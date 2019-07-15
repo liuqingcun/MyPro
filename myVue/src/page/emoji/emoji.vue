3881799cf72245d16be18bfbd449414cdb35a191
liuqingcunPC <liuqingcun_code@163.com>
2019-07-15 09:13:52 +0800

    -datepicker

---
+++ myVue/src/page/emoji/emoji.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <VueEmoji ref="emoji" width="500" height="200" placeholder="请输入" @input="onInput" />
+    <div>{{inputVal}}</div>
+  </div>
+</template>
+
+<script>
+import VueEmoji from '../../components/emoji/VueEmoji'
+export default {
+  name: 'Emoji',
+  components:{
+    VueEmoji
+  },
+  data () {
+    return {
+      inputVal:""
+    }
+  },
+  methods:{
+    onInput(event) {
+      console.log(event.data);
+      this.inputVal = event.data;
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ myVue/src/page/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div class="linkList">
+      <router-link v-for="(item,index) in linkList" :to='item.url' :key="item">
+        <div class="linkItem" >{{index+1}}、{{item.title}}</div>
+      </router-link>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      linkList: [{
+        title:"emoji表情",
+        url:"/emoji"
+      }]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
